2dbf0f10e9aa0ba3d2ba067fd04d4370bc6f26c9
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-11-28 13:42:39 -0500

    Fixed date issue

--- plot1.R
+++ plot1.R
@@ -1,13 +1,20 @@
 # load data and convert Date, Time to Date/Time classes 
 
 power <-read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
-power <-subset(power, power$Date=="1/2/2007"|power$Date=="2/2/2007")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subPower <-subset(power, power$Date=="1/2/2007"|power$Date=="2/2/2007")
 
-power$Date <-as.Date(power$Date, format="%d/%m/%Y")
-power$Time <-strptime(power$Time, format="%H:%M:%S")
+subPower$Date <-as.Date(subPower$Date, format="%d/%m/%Y")
+subPower$Time <-strptime(subPower$Time, format="%H:%M:%S")
+
+# format date to Jan and Feb 2007 (not current date)
+subPower[1:1440,"Time"] <- format(subPower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subPower[1441:2880,"Time"] <- format(subPower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
 
 # create plot 1 
-with(power, hist(Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
+with(subPower, hist(Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
 
 # save plot as .png file
 png(filename="plot1.png", width = 480, height=480)
