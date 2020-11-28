51f449811c15a86c51fabd929c5bddd65d9efe94
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-11-28 14:57:57 -0500

    Edit plot 4

--- plot4.R
+++ plot4.R
@@ -16,14 +16,14 @@ subPower[1441:2880,"Time"] <- format(subPower[1441:2880,"Time"],"2007-02-02 %H:%
 png("plot4.png", width = 480, height=480)
 par(mfrow=c(2,2))
 with(subPower, {
-  plot(Time,Global_active_power,type="l",xlab="",ylab="Global Active Power") 
-  plot(Time,Voltage,type="l",xlab="datetime") 
+  plot(subPower$Time,subPower$Global_active_power,type="l",xlab="",ylab="Global Active Power") 
+  plot(subPower$Time,subPower$Voltage, ylab="Voltage", xlab="datetime")
   plot(subPower$Time,subPower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
     with(subPower,lines(Time,Sub_metering_1, col="black"))
     with(subPower,lines(Time,Sub_metering_2,col="red"))
     with(subPower,lines(Time,Sub_metering_3,col="blue"))
     legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
-  plot(subPower$Time,subPower$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+  plot(subPower$Time,subPower$Global_reactive_power,type="l",xlab="datetime",ylab="Global_rective_power") 
 })
 dev.off()
 
